4674d2253e31f5573587dc8dbdaf57287d084630
Jeff Karr <jkarr001@gmail.com>
2018-12-29 11:36:24 -0600

    revised favicon to resolve deploy issue on heroku.

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
-const favicon = require("express-favicon");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
-
-app.use(favicon(__dirname + '/build/favicon.ico'));
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
