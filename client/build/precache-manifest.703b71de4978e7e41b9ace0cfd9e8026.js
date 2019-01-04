30395c18e4b33356ae8f229a3ba310518d4019bd
Jeff Karr <jkarr001@gmail.com>
2019-01-04 11:26:39 -0600

    adding build folder to client to resolve heroku load issue.

---
+++ client/build/precache-manifest.703b71de4978e7e41b9ace0cfd9e8026.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "54b9db02733c3ef37173",
+    "url": "/static/css/main.db3008af.chunk.css"
+  },
+  {
+    "revision": "54b9db02733c3ef37173",
+    "url": "/static/js/main.54b9db02.chunk.js"
+  },
+  {
+    "revision": "54b99ab43dd588138d8b",
+    "url": "/static/js/1.54b99ab4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73dd2d734eab3a543cff1e880155b53d",
+    "url": "/static/media/coffee-category-image.73dd2d73.jpg"
+  },
+  {
+    "revision": "a89ad3f2849cec17c19d723f35b9a3fa",
+    "url": "/static/media/tea-category-image.a89ad3f2.jpg"
+  },
+  {
+    "revision": "ed9acbc6b9047595c7461c3ee727bd83",
+    "url": "/static/media/accessories-category-image.ed9acbc6.jpg"
+  },
+  {
+    "revision": "6e3d4457412496fabad22a8f33de7909",
+    "url": "/index.html"
+  }
+];
